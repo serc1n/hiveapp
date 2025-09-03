5475faa813f8a4f66c75ce2cbc2a5e15d25e9b5f
Serc <sercan@Sercs-MacBook-Pro.local>
2025-09-03 22:09:49 +0200

    Switch to Twitter API v1.1 to avoid rate limits

--- lib/auth.ts
+++ lib/auth.ts
@@ -7,7 +7,6 @@ export const authOptions: NextAuthOptions = {
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID!,
       clientSecret: process.env.TWITTER_CLIENT_SECRET!,
-      version: '2.0',
     }),
   ],
   callbacks: {
